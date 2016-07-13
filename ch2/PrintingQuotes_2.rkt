562411a8d0469a101936481cc688e3afb4380030
Chris Grayson <chris@activeprospect.com>
2016-07-13 16:01:19 -0500

    add exercise 3; racket

---
+++ ch2/PrintingQuotes_2.rkt
@@ -0,0 +1,13 @@
+#lang racket
+(require readline/readline)
+
+(struct quote (words quoter))
+
+(define quotes
+  (list (quote "Your work is to discover your work and then, with all your heart, to give yourself to it." "Buddha")
+        (quote "We are what we repeatedly do; excellence, then, is not an act but a habit." "Aristotle")
+        (quote "Do your work, and you shall reinforce yourself." "Emerson")))
+
+(andmap (lambda (q)
+         (displayln (string-append (quote-quoter q) " says, \"" (quote-words q) "\"")))
+         quotes)
